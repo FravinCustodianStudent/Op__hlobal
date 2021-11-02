60697c5d76ce488b483f36f6db627e8d6397ea20
Fesak Andrii <3114226@gmail.com>
2021-11-02 13:17:25 +0200

    Initial commit

--- js/main.js
+++ js/main.js
@@ -9,3 +9,28 @@ document.addEventListener('animationend', function (e) {
     e.target.classList.remove('did-fade-in');
   }
 });
+document.addEventListener('DOMContentLoaded', () => {
+  hljs.initHighlightingOnLoad();
+
+  const codeBlock = document.getElementById('code');
+  const copyButton = document.getElementById('copy-button');
+  const copySuccess = document.getElementById('copy-success');
+
+  const copyTextHandler = () => {
+    const text = codeBlock.innerText;
+
+    navigator.clipboard.writeText(text).then(
+      () => {
+        copySuccess.classList.add('show-message');
+        setTimeout(() => {
+          copySuccess.classList.remove('show-message');
+        }, 2500);
+      },
+      () => {
+        console.log('Error writing to the clipboard');
+      }
+    );
+  };
+
+  copyButton.addEventListener('click', copyTextHandler);
+});
